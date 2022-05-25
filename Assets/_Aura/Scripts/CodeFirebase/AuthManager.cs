a52fde8c186fca5b55527b151a3a97d5da628fd7
Auravision <auravision.africa@gmail.com>
2022-05-25 21:40:08 +0300

    Firebase authentication SDK imported

---
+++ Assets/_Aura/Scripts/CodeFirebase/AuthManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using TMPro;
+
+public class AuthManager : MonoBehaviour
+{
+    public TMP_InputField emailInput, passwordInput;
+
+
+}
